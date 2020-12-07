949c843ec23d1cab7a663719be86002f946aec2f
Patrick <patrick.lloyd@theproducebox.com>
2020-12-07 00:02:21 -0500

    styling page and adding functions

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,8 +1,7 @@
 $(function () {
   // add new
-  $('.create-form').on('submit', function (event) {
+  $('.create-form').on('submit', (event) => {
     event.preventDefault();
-
     const newBurger = {
       burger_name: $('#newburger').val().trim(),
       devoured: 0,
@@ -11,10 +10,18 @@ $(function () {
     $.ajax('/api/burgers', {
       type: 'POST',
       data: newBurger,
-    }).then(function () {
+    }).then(() => {
       console.log('Added new burger');
       // refresh page
       location.reload();
     });
   });
 });
+
+function play() {
+  var audio = new Audio(
+    // '/assets/sounds/woohoo.mp3'
+    '/assets/sounds/mburger.mp3'
+  );
+  audio.play();
+}
