c7992bd0170ac633b59d8faaad61718319930ebe
Patrick <patrick.lloyd@theproducebox.com>
2020-12-07 11:43:40 -0500

    adding delay for reload, updating fonts

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -6,22 +6,24 @@ $(function () {
       burger_name: $('#newburger').val().trim(),
       devoured: 0,
     };
-
     $.ajax('/api/burgers', {
       type: 'POST',
       data: newBurger,
     }).then(() => {
       console.log('Added new burger');
-      // refresh page
-      location.reload();
+      setTimeout(function () {
+        location.reload();
+      }, 1000);
     });
   });
 });
 
-function play() {
-  var audio = new Audio(
-    // '/assets/sounds/woohoo.mp3'
-    '/assets/sounds/mburger.mp3'
-  );
+function playWoohoo() {
+  var audio = new Audio('/assets/sounds/woohoo.mp3');
+  audio.play();
+}
+
+function playBurger() {
+  var audio = new Audio('/assets/sounds/mburger.mp3');
   audio.play();
 }
